2c8e69ff06497e0b1f50ec729b15e83506caa895
nk-on <2nkoz@proton.me>
2025-04-23 07:12:52 -0700

    Feat:Displaying fetched data inside showcase component

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -1,16 +1,18 @@
 import Heading from "./Heading";
 import { useContext } from "react";
 import { DictionaryConext } from "../Context";
+import Showcase from "./Showcase";
 export default function Container() {
   const {darkModeOn} = useContext(DictionaryConext);
   return (
     <div
-      className={`w-[100vw] h-[100vh] flex justify-center ${darkModeOn && 'bg-[#050505]'} items-center ${
+      className={`max-w-[100vw] h-[100vh] flex justify-center ${darkModeOn && 'bg-[#050505]'} items-center ${
         darkModeOn && "dark"
       }`}
     >
       <div className="lg:w-[50%] w-[80%] h-[90%]">
         <Heading />
+        <Showcase />
       </div>
     </div>
   );
---
+++ src/Components/Showcase.tsx
@@ -0,0 +1,65 @@
+import { useContext} from "react";
+import { DictionaryConext } from "../Context";
+export default function Showcase() {
+  const { data } = useContext(DictionaryConext);
+  if (data) {
+    const meanings = data["meanings"]?.[0]?.definitions;
+    const synonym = data["meanings"]?.[0]?.synonyms?.[0];
+    const definition = data["meanings"]?.[1]?.definitions?.[0]?.["definition"];
+    const example = data["meanings"]?.[1]?.definitions?.[0]?.["example"];
+    const sourceUrls = data["sourceUrls"];
+    return (
+      <>
+        <div className="flex flex-col gap-[40px] w-[100%]">
+          <div className="flex justify-between items-center w-[100%] ">
+            <div>
+              <h1 className="font-bold text-[64px]">{data.word}</h1>
+              <p className="text-[#A445ED]">{data.phonetic}</p>
+            </div>
+            <div className="h-[20%] flex items-center">
+              <img src="public/icon-play.svg" className="cursor-pointer" />
+            </div>
+          </div>
+          <SectionTitle title={"noun"} />
+          <div className="flex flex-col">
+            <h2 className="text-[#757575] text-[20px]">Meaning</h2>
+            <ul className="marker:text-[#8F19E8] px-[30px]">
+              {meanings?.map((element, idx) => {
+                return (
+                  <li key={idx} className="list-disc">
+                    {element.definition}
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="text-[#757575] text-[20px] mt-[50px] font-normal">
+              Synonyms: <span className="text-[#8F19E8] font-bold">{synonym}</span>
+            </div>
+          </div>
+          <SectionTitle title="verb" />
+          <div className="flex flex-col">
+            <h2 className="text-[#757575] text-[20px]">Meaning</h2>
+            <ul className="marker:text-[#8F19E8] px-[30px]">
+              <li className="list-disc">
+                <div className="flex flex-col">
+                  {example} <span className="text-[#757575]">"{example}"</span>
+                </div>
+              </li>
+            </ul>
+            <div className="flex flex-col gap-[20px]">
+              <div className="w-[90%] h-[1px] bg-[#E9E9E9]"></div>
+              <div className="text-[#757575] flex gap-[10px]">
+                Soruce{" "}
+                <span className="text-[#2D2D2D] flex gap-[10px]">
+                  <a href={`${sourceUrls}`}>{sourceUrls}</a>
+                  <img src="public/icon-new-window.svg" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return <></>;
+}
