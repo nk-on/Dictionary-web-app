39ee135b6f25292b4648108dd677f5caf47162c6
nk-on <2nkoz@proton.me>
2025-04-23 22:53:00 -0700

    Increasing emoji size on error page

--- src/Components/ErrorPage.tsx
+++ src/Components/ErrorPage.tsx
@@ -1,8 +1,8 @@
 export default function ErrorPage() {
   return (
     <div className="w-[100%] flex flex-col gap-[10px] justify-center items-center mt-[30px]">
-      <div>😕</div>
-      <h1>No definitions found</h1>
+      <div className="text-[64px]">😕</div>
+      <h1 className="dark:text-[#fff]">No definitions found</h1>
       <p className="w-[70%] text-[#757575]">
         Sorry pal, we couldn't find definitions for the word you were looking for. You can
         try the search again at later time or head to the web instead.
